52c1831d1348be8bfe46b81c048e805c24023b30
VynoDePal <odjokevyn@gmail.com>
2025-09-01 11:34:44 +0100

    Refactor ModulePage to use renderModulePage function for improved readability and maintainability

--- dashcraft-app/src/app/modules/[key]/page.tsx
+++ dashcraft-app/src/app/modules/[key]/page.tsx
@@ -12,6 +12,7 @@ import {ChatsPage} from '@/modules/chats/ChatsPage'
 import {MonitoringPage} from '@/modules/monitoring/MonitoringPage'
 import {AnalyticsPage} from '@/modules/analytics/AnalyticsPage'
 import {LanguagesPage} from '@/modules/languages/LanguagesPage'
+import {SettingsPage} from '@/modules/settings/SettingsPage'
 import {getTranslations} from 'next-intl/server'
 
 interface ModulePageProps {
@@ -25,35 +26,50 @@ interface ModulePageProps {
  */
 type AwaitedModulePageProps = {params: Promise<ModulePageProps['params']>}
 
+/**
+ * renderModulePage
+ * Renvoie le composant de page correspondant à la clé du module.
+ */
+function renderModulePage(key: DashboardModule['key']) {
+	switch (key) {
+		case 'users':
+			return <UsersPage />
+		case 'notifications':
+			return <NotificationsPage />
+		case 'emails':
+			return <EmailsPage />
+		case 'feedbacks':
+			return <FeedbacksPage />
+		case 'payments':
+			return <PaymentsPage />
+		case 'subscriptions':
+			return <SubscriptionsPage />
+		case 'calendar':
+			return <CalendarPage />
+		case 'apis':
+			return <ApisPage />
+		case 'monitoring':
+			return <MonitoringPage />
+		case 'analytics':
+			return <AnalyticsPage />
+		case 'languages':
+			return <LanguagesPage />
+		case 'chats':
+			return <ChatsPage />
+		case 'settings':
+			return <SettingsPage />
+		default:
+			return null
+	}
+}
 export default async function ModulePage({params}: AwaitedModulePageProps) {
 	const {key} = await params
 	const t = await getTranslations('pages.generic')
+	const page = renderModulePage(key)
 	return (
 		<DashboardLayout>
-			{key === 'users' ? (
-				<UsersPage />
-			) : key === 'notifications' ? (
-				<NotificationsPage />
-			) : key === 'emails' ? (
-				<EmailsPage />
-			) : key === 'feedbacks' ? (
-				<FeedbacksPage />
-			) : key === 'payments' ? (
-				<PaymentsPage />
-			) : key === 'subscriptions' ? (
-				<SubscriptionsPage />
-			) : key === 'calendar' ? (
-				<CalendarPage />
-			) : key === 'apis' ? (
-				<ApisPage />
-			) : key === 'monitoring' ? (
-				<MonitoringPage />
-			) : key === 'analytics' ? (
-				<AnalyticsPage />
-			) : key === 'languages' ? (
-				<LanguagesPage />
-			) : key === 'chats' ? (
-				<ChatsPage />
+			{page ? (
+				page
 			) : (
 				<div className='rounded-lg border border-white/10 bg-white/5 p-4'>
 					<p className='text-sm text-white/80'>
