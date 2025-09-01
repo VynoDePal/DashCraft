125ab4deda95eef5525b9b21b8b9aa12064e439b
VynoDePal <odjokevyn@gmail.com>
2025-09-01 11:34:10 +0100

    Update DashboardLayout to manage sidebar visibility with Redux state

--- dashcraft-app/src/components/layout/DashboardLayout.tsx
+++ dashcraft-app/src/components/layout/DashboardLayout.tsx
@@ -3,6 +3,8 @@
 import {type ReactNode} from 'react'
 import {Sidebar} from '@/components/layout/Sidebar'
 import {Topbar} from '@/components/layout/Topbar'
+import {useSelector} from 'react-redux'
+import type {RootState} from '@/store/store'
 
 /**
  * DashboardLayout
@@ -13,12 +15,21 @@ export interface DashboardLayoutProps {
 }
 
 export function DashboardLayout({children}: DashboardLayoutProps) {
+	const sidebarOpen = useSelector((s: RootState) => s.ui.sidebarOpen)
 	return (
-		<div className='min-h-screen grid grid-rows-[auto_1fr] md:grid-cols-[16rem_1fr]'>
+		<div
+			className={`min-h-screen grid grid-rows-[auto_1fr] ${
+				sidebarOpen ? 'md:grid-cols-[16rem_1fr]' : 'md:grid-cols-1'
+			}`}
+		>
 			<div className='md:col-span-2'>
 				<Topbar />
 			</div>
-			<aside className='hidden md:block'>
+			<aside
+				className={sidebarOpen ? 'hidden md:block' : 'hidden'}
+				data-testid='sidebar-container'
+				aria-hidden={!sidebarOpen}
+			>
 				<Sidebar />
 			</aside>
 			<main className='p-4'>{children}</main>
